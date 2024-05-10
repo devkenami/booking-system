23bb43f841a11208aa9bada6c9a37789d9e750f4
babayaga <74756666+CarlSaqui29@users.noreply.github.com>
2024-05-10 23:59:02 +0800

    [#6] - Create a login page design based on mockup (#8)

--- index.php
+++ index.php
@@ -43,26 +43,57 @@ if(isset($_POST['submit'])){
     <title>login form</title>
 
     <!--custom css file link -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 <body>
 
-<div class="form-container">
+    <div class="form-container landing_page">
+    <div class="row">
+        <div class="col left_side">
+        <img class="brand-logo" src="./img/logo.jpeg" alt="Logo">
+        
+        <div class="text-center">
+            <h1 class="mt-3">Animal Life Clinic and Supply</h1>
+            <img class="mt-5 dog" src="./img/dog.png" alt="dog">
+        </div>
+        </div>
+        <div class="col right_side d-flex justify-content-center align-items-center">
+            <center>
+                <div class="pink_header">
+                    <h3>Sign In</h3>
+                </div>
+                
+                <form action="" method="post">
+                    <?php
+                        if(isset($error)){
+                            foreach($error as $error){
+                                echo '<span class="error-msg">'.$error.'</span>';
+                            };
+                        };
+                    ?>
+                    <p class="mb-4">Enter your information below</p>
 
-    <form action="" method="post">
-        <h3>login now</h3>
-        <?php
-        if(isset($error)){
-            foreach($error as $error){
-                echo '<span class="error-msg">'.$error.'</span>';
-            };
-        };
-        ?>
-        <input type="email" name="email" required placeholder="enter your email">
-        <input type="password" name="password" required placeholder="enter your password">
-        <input type="submit" name="submit" value="login now" class="form-btn">
-        <p>don't have an account? <a href="register_form.php">register now</a></p>
-    </form>
-</div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"><i class="fa-regular fa-envelope"></i></span>
+                        <input type="email" name="email" class="form-control" placeholder="Enter your email" aria-label="Username" aria-describedby="basic-addon1" required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-lock"></i></span>
+                        <input type="password" name="password" class="form-control" placeholder="Enter your password" aria-label="password" aria-describedby="basic-addon1" required>
+                    </div>
+                    <div class="input-group mt-4 mb-3 d-flex justify-content-center">
+                        <input type="submit" name="submit" class="btn btn-pink w-50" value="Sign In">
+                    </div>
+                    <center>
+                        <p class="mt-4">don't have an account? <a href="register_form.php" class="text-pink">register now</a></p>
+                    </center>
+                </form>
+            </center>
+        </div>
+    </div>
+    </div>
 </body>
 </html>
