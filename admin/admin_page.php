ae482b37bf1e2de2abdb4df095c3a83931e8ac20
babayaga <74756666+CarlSaqui29@users.noreply.github.com>
2024-05-10 22:00:26 +0800

    change folder structure (#7)

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -6,7 +6,7 @@
     <title>admin page</title>
 
     <!--custom css file link -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 <body>
     
@@ -16,9 +16,9 @@
         <h3>hi welcome to, <span>Collins Animal Clinic</span></h3>
         <h1>FurEver Fit <span></span></h1>
         <p>this is an admin page</p>
-        <a href="index.php" class="btn">login</a>
-        <a href="register_form.php" class="btn">register</a>
-        <a href="logout.php" class="btn">logout</a>
+        <a href="../index.php" class="btn">login</a>
+        <a href="../register_form.php" class="btn">register</a>
+        <a href="../logout.php" class="btn">logout</a>
     </div>
 
 </div>
